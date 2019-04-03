0811d172efa1eb4e65145ed8ef520dbbc9ed485a
Yuki Takizawa <y.takizawa64@gmail.com>
2019-04-03 23:00:27 +0900

    Fix CI script

---
+++ script/check-Dockerfile-diff.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+echo "Check differences of Dockerfile between official and this repo"
+wget https://raw.githubusercontent.com/GoogleChrome/puppeteer/master/docs/troubleshooting.md
+
+function getLineNum() {
+  grep -nF "$1" troubleshooting.md | cut -d':' -f1;
+}
+
+lineNumAtSectionTitle=$(getLineNum '## Running Puppeteer in Docker')
+
+lineNumFrom=$(
+  for lineNum in $(getLineNum '```Dockerfile'); do
+    if [ $lineNum -gt $lineNumAtSectionTitle ]; then
+      echo $(($lineNum + 1))
+      break
+    fi
+  done
+)
+
+lineNumTo=$(
+  for lineNum in $(getLineNum '```'); do
+    if [ $lineNum -gt $lineNumFrom ]; then
+      echo $(($lineNum - 1))
+      break
+    fi
+  done
+)
+
+diff <(sed -n ${lineNumFrom},${lineNumTo}p troubleshooting.md) Dockerfile
